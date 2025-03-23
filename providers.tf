6be9f30153a7f454f695222bb8db2f5166a7d503
AkashPratap <71942494+AkashPratap@users.noreply.github.com>
2025-03-23 16:56:47 +0530

    backend fixes

--- providers.tf
+++ providers.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
     features {}
-    subscription_id = "38bd44d5-d91c-4f34-8f1e-c946d6697ae4"
-    client_id = "64678df4-13ca-4102-bd58-5cc5fb64a70f"
-    client_secret = "cdl8Q~NF7cl2BIQtEzTbowWxAr9yuaIbCiksvdkW"
-    tenant_id = "d0f66b2a-6d50-4e11-b7a6-e0fe38d06609"
+    subscription_id = var.subscription_id
+    client_id = var.client_id
+    client_secret = var.client_secret
+    tenant_id = var.tenant_id
 
-    use_msi = false
+    skip_provider_registration = true
 }
